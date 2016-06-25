abf1892955508eb1a038c7b45aefa9e864c4fced
Steven Avon <savon40@gmail.com>
2016-06-25 22:16:42 +0000

    email fix

--- app/mailers/new_comment.rb
+++ app/mailers/new_comment.rb
@@ -1,5 +1,5 @@
 class NewComment < ActionMailer::Base
-    default :from "savon40@gmail.com"
+    default from: "savon40@gmail.com"
     
     def send_email (scenario)
         mail(:to =>scenario.user.email, :subject => 'A Comment has been created on your Scenario: ' +  scenario.description)
